02be46c3c633b6bb2db0a4a6bf957467d30eb608
AbdulRehman4481 <abdulrehmanmughal4481@gmail.com>
2024-08-25 13:04:44 +0100

    feat:resolve conflict utilti

--- src/utilites/Instance.tsx
+++ src/utilites/Instance.tsx
@@ -2,7 +2,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: 'medicare-app-ten.vercel.app',
+  baseURL: 'https://medicare-app-ten.vercel.app/',
 });
 
 export default instance;
